9350c81eb898817f5663e6737664f3590b78ffc5
Beosti <ziroxismail@gmail.com>
2024-08-01 23:15:02 +0200

    made an upgrade 'page'

---
+++ wizard/scenes/userinterface/upgrade.gd
@@ -0,0 +1,16 @@
+class_name Upgrade
+extends Control
+
+@export var view : UserInterface.Views;
+@export var user_interface : UserInterface;
+
+func _ready() -> void:
+	visible = false;
+	user_interface.navigation_requested.connect(_on_navigation_request)
+	
+func _on_navigation_request(requested_view : UserInterface.Views) -> void:
+	if requested_view == view:
+		visible = true;
+		return;
+		
+	visible = false;
--- wizard/scenes/userinterface/user_interface.gd
+++ wizard/scenes/userinterface/user_interface.gd
@@ -4,6 +4,7 @@ extends Control
 enum Views {
 	GENERATOR,
 	CLICKER,
+	UPGRADE,
 }
 
 signal navigation_requested(view : Views)
@@ -15,3 +16,6 @@ func _on_clicker_link_pressed() -> void:
 
 func _on_generator_link_pressed() -> void:
 	navigation_requested.emit(Views.GENERATOR)
+
+func _on_upgrade_link_pressed() -> void:
+	navigation_requested.emit(Views.UPGRADE);
